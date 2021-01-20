756670bd077627f66f5761858454bb0f5e3fd3c2
taratimmerman <timmermantara@gmail.com>
2021-01-19 19:06:27 -0700

    mvp complete

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { removeFeature } from '../actions/carsalesActions';
+import { connect } from 'react-redux';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        className="button"
+        onClick={() => {
+          props.removeFeature(props.feature);
+        }}
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car,
+  };
+};
+export default connect(mapStateToProps, { removeFeature })(AddedFeature);
